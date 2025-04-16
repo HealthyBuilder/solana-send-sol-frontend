d02493c0f3d336c9e0620f1296e1b80bb907bb02
HealthyBuilder <maqiyuan162@163.com>
2025-04-16 20:13:36 +0800

    feat: send sol

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import styles from '../styles/Home.module.css'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import Image from 'next/image'
 
 export const AppBar: FC = () => {
@@ -8,6 +9,7 @@ export const AppBar: FC = () => {
             <Image src="/solanaLogo.png" height={30} width={200} />
             <span>Wallet-Adapter Example</span>
             <button>Connect</button>
+            <WalletMultiButton />
         </div>
     )
 }
